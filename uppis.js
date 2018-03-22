84aed612337e894b06c93199733c3b1695fd6a3a
tapsaman <tapio.valkama@student.hamk.fi>
2018-03-22 16:27:11 +0200

    'nameless-commit'

---
+++ uppis.js
@@ -0,0 +1,34 @@
+const { execSync } = require('child_process');
+
+console.log(process.argv)
+
+const commitMsg = process.argv[2] 
+	? process.argv[2].slice(2)
+	: "nameless-commit"
+
+const commands = [
+	"git add .",
+	"git commit -m '"+commitMsg+"'",
+	"npm version patch",
+	"node parseReadme.js",
+	"git add .",
+	"git commit --amend --no-edit",
+	"git push",
+	"npm publish"
+]
+
+try {
+
+	for (let i=0; i < commands.length; i++) {
+		console.log( "> " + commands[i] )
+		let stdout = execSync( commands[i] )
+		console.log( stdout )
+	}
+
+}
+catch(ex) {
+
+	console.error("Error:")
+	console.error(ex)
+
+}
