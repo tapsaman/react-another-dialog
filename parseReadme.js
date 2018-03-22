284d51bf857dedf30b0fe3b17f34c064b2ea7092
tapsaman <tapio.valkama@student.hamk.fi>
2018-03-22 16:47:53 +0200

    'nameless-commit'

--- parseReadme.js
+++ parseReadme.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 const glob = require('glob-fs')({ gitignore: true })
 const package = require('./package.json')
 
-const separator = "\n\n*************************\n\n\n\n"
+const separator = "<br><br><br>*************************<br><br><br>"
 
 var output = "",
 	foundFiles = [], 
@@ -19,7 +19,7 @@ function getHeader() {
 			+ "\n+ **date:** " + getDateString()
 			+ "\n+ **license:** " + package.license
 			+ "\n+ **author:** " + package.author
-			+ "\n\n\n\n*** WORK IN PROGRESS ***\n\n"
+			+ "\n<br><br>*** WORK IN PROGRESS ***<br><br>"
 	}
 	return "";
 }
@@ -55,7 +55,7 @@ function extract(file, data) {
 			output += separator
 
 			if (iter === 0)
-				output += separator + "*In file ["+file.basename+"]("+file.relative.replace(/\\/g,"//")+")*\n"
+				output += "*In file ["+file.basename+"]("+file.relative.replace(/\\/g,"//")+")*\n"
 
 			output += res[1]
 		}
@@ -100,63 +100,4 @@ function getDateString()
 	str = str.replace( /\/(\d(?!\d))/g, "/0$1" )
 
 	return str
-}
-
-
-
-function __extract(file, contents) {
-
-	if (err) {
-		console.log(err)
-	}
-	else {
-
-		const extractRegex = /\^\^\^\^([\s\S]+?)\^\^\^\^/g
-		const file = foundFiles[extractCount]
-		var res, iter = 0
-
-		do {
-			res = extractRegex.exec(data)
-
-			if (res) {
-				output += separator
-
-				if (iter === 0)
-					output += "*In file ["+file.basename+"]("+file.relative+")*\n"
-
-				output += res[1]
-			}
-
-			iter++;
-
-		} while(res)
-	}
-
-	if (++extractCount === foundFiles.length)
-	{
-		console.log("End of search.")
-
-		if (output.length === 0)
-		{
-			console.log("Found nothing to extract...")
-			return
-		}
-
-		if (package) {
-			let header = "# AnotherDialog document\n\n"
-				+ package.description
-				+ "\n\n*npm name*: " + package.name
-				+ "\n\n*version*: " + package.version
-				+ "\n\n*date*: " + getDateString()
-				+ "\n\n*license*: " + package.license
-				+ "\n\n*author*: " + package.author + "\n\n"
-
-			output = header + output
-		}
-
-		fs.writeFile('genREADME.md', output, function(err) {
-		    if (err) throw err;
-		    console.log("Generated .md")
-		})
-	}
 }
